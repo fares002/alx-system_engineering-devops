607781acb64534e6b9c1754e477a9fd4bc70fc7d
fares <you@fares002.com>
2024-03-22 18:48:48 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#this puppet manifest
+
+file{'/tmp/school':
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I Love puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+<h1>0x0A. Configuration management</h1>
+
