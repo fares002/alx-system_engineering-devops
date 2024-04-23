d56890464b4124338394d21e867f1f00319a338f
fares <fares002@example.com>
2024-04-23 15:59:41 -0700

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,9 +2,8 @@
 """a given employee ID,
 returns information about his/her
 TODO list progress."""
-import sys
 import requests
-
+import sys
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
