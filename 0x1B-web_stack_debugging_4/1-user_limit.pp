70d32364e892f58c506223aea0f815f4f3ff09de
fares <fares002@example.com>
2024-05-16 10:50:43 -0700

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,21 @@
-#Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
-
-# Increase hard file limit for holbrton user
-
-exec { 'increase-hard-file-limit-for-holbrton-user':
-  command => 'sed -i "/^holberton hard/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+# Ensure the holberton user exists
+user { 'holberton':
+  ensure => 'present',
 }
 
-# Increase soft file limit for holbrton user
-
-exec { 'increase-soft-file-limit-for-holbrton-user':
-  command => 'sed -i "/^holberton soft/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+# Increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/^holberton.*hard.*nofile/s/5/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin', '/sbin'],
+  unless  => 'grep -q "holberton.*hard.*nofile.*50000" /etc/security/limits.conf',
+  require => User['holberton'],
 }
+
+# Increase soft file limit for holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/^holberton.*soft.*nofile/s/4/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin', '/sbin'],
+  unless  => 'grep -q "holberton.*soft.*nofile.*50000" /etc/security/limits.conf',
+  require => User['holberton'],
+}
+
